860d91358c0aa9d400a4f67b072112f5bc0c602a
Lightonik <wwwwitaly@gmail.com>
2025-01-06 23:34:50 +0300

    camera btn

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,6 +1,18 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img
+        class="full-width"
+        src="https://sun9-32.userapi.com/impf/c830608/v830608531/15cbb8/H2RclojnmtQ.jpg?size=2560x1920&quality=96&sign=cf30bf10bfdb2e90399e0685d48a3ac8&type=album">
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        round
+        color="grey-10"
+        icon="eva-camera" 
+        size="lg"
+      />
+    </div>
   </q-page>
 </template>
 
@@ -9,3 +21,9 @@ export default {
   name: 'PageCamera'
 }
 </script>
+
+<style lang="sass">
+  .camera-frame
+    border: 2px solid $grey-10
+    border-radius: 10px
+</style>
