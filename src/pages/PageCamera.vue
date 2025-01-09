a5f56d841943eea6e36f561784937097969b3d05
Lightonik <wwwwitaly@gmail.com>
2025-01-09 18:06:01 +0300

    post data

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="constrain q-pa-md">
+  <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
       <img
         class="full-width"
@@ -8,17 +8,52 @@
     <div class="text-center q-pa-md">
       <q-btn
         round
-        color="grey-10"
+        color="primary"
         icon="eva-camera" 
         size="lg"
       />
+    </div>
+    <div class="row justify-center q-ma-md">
+      <q-input
+        v-model="post.caption"        
+        label="Caption"
+        class="col col-sm-6"
+        dense
+      />
+    </div>
+    <div class="row justify-center q-ma-md">
+      <q-input
+        v-model="post.location"
+        label="Location"
+        class="col col-sm-6"
+        dense
+      >
+        <template v-slot:append>
+          <q-btn round dense flat icon="place" />
+        </template>
+      </q-input>
+    </div>
+    <div class="row justify-center q-mt-lg">
+      <q-btn unelevated rounded color="primary" label="Post Image" />
     </div>
   </q-page>
 </template>
 
 <script>
+import { uid } from 'quasar'
 export default {
-  name: 'PageCamera'
+  name: 'PageCamera',
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: '',
+        location: '',
+        photo: null,
+        date: Date.now()
+      }
+    }
+  }
 }
 </script>
 
@@ -27,3 +62,5 @@ export default {
     border: 2px solid $grey-10
     border-radius: 10px
 </style>
+
+
