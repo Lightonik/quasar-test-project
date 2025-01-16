cd675c58946083a0fe2a0e76ef0ad0d29e5a9db1
Lightonik <wwwwitaly@gmail.com>
2025-01-16 19:30:44 +0300

    init camera

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,9 +1,14 @@
 <template>
   <q-page class="constrain-more q-pa-md">
     <div class="camera-frame q-pa-md">
-      <img
+
+      <video
+        ref="video"
         class="full-width"
-        src="https://sun9-32.userapi.com/impf/c830608/v830608531/15cbb8/H2RclojnmtQ.jpg?size=2560x1920&quality=96&sign=cf30bf10bfdb2e90399e0685d48a3ac8&type=album">
+        autoplay></video
+      >
+     
+      
     </div>
     <div class="text-center q-pa-md">
       <q-btn
@@ -53,6 +58,18 @@ export default {
         date: Date.now()
       }
     }
+  },
+  methods: {
+    initCamera () {
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(stream => {
+        this.$refs.video.srcObject = stream
+      })
+    }
+  },
+  mounted() {
+    this.initCamera()
   }
 }
 </script>
